c0e27390f45b05bad611afc7766514e50bdec082
Keagan Barnes <barnes.keagan9@gmail.com>
2019-11-17 12:40:12 -0700

    Everything styled, still need to be able to show songs on bilboard and get faker to work

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,15 +3,15 @@ include Faker
 
 1.times do
   Artist.create(
-    name: Faker::Music.band,
+    name: Faker::Music.band
     genre: Faker::Music.genre
   )
 end
 
 10.times do
   Song.create(
-    name: Faker::RockBand.name,
-    genre: Faker::Music.genre,
+    name: Faker::RockBand.name
+    genre: Faker::Music.genre
     album: Faker::Music.album
   )
 end
