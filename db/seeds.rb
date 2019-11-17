520be0d298a00163fcdc670a88a2de60689dd0e0
Keagan Barnes <barnes.keagan9@gmail.com>
2019-11-17 13:56:49 -0700

    all styled, just need to generate fake data and pass songs to the billboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,18 @@
 require 'faker'
 include Faker
 
-1.times do
-  Artist.create(
-    name: Faker::Music.band
-    genre: Faker::Music.genre
-  )
-end
-
 10.times do
-  Song.create(
-    name: Faker::RockBand.name
+  Artist.create
+    name: Faker::Music.band,
     genre: Faker::Music.genre
+
+
+100.times do
+  Song.create
+    name: Faker::Name.name,
+    genre: Faker::Music.genre,
     album: Faker::Music.album
-  )
+  
 end
 
 
