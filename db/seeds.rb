7633ee5c284271b85d6f09c3d085699cfcfb8962
Keagan Barnes <barnes.keagan9@gmail.com>
2019-11-16 23:25:01 -0700

    The correct files, songs won't post to billboard and I can't get them to be tied to one specific artist

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-100.times do
+require 'faker'
+include Faker
+
+1.times do
   Artist.create(
-    name: Faker::Artist.name,
+    name: Faker::Music.band,
+    genre: Faker::Music.genre
+  )
+end
+
+10.times do
+  Song.create(
+    name: Faker::RockBand.name,
     genre: Faker::Music.genre,
+    album: Faker::Music.album
   )
 end
 
